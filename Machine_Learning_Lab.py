520c0415e0e902a8ba30efde572999e5b0b1857b
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2021-07-18 21:45:25 +0530

    Update Machine_Learning_Lab.py

--- Machine_Learning_Lab.py
+++ Machine_Learning_Lab.py
@@ -1,29 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1 a.]:
+# In[1 a.]:missiing values
 
 
 import pandas as pd
-import numpy as nm
-import matplotlib.pyplot as mtp
-data_set=pd.read_csv('Data.csv')
-x= data_set.iloc[:,:-1].values
-print(data_set)
-print(x)
-data_set['Age'].fillna(value=data_set['Age'].mean(), inplace=True)
-print(data_set)
-data_set=data_set.dropna()
-print(data_set)
-from sklearn.preprocessing import LabelEncoder
-label_encoder_x= LabelEncoder()
-x[:,0]= label_encoder_x.fit_transform(x[:, 1])
-print(x)
-x= data_set.iloc[:, 1:3].values
-print("\nOriginal data values : \n", x)
+df = pd.read_csv("Data.csv")
+df.fillna(0,inplace = False)
+x= df.iloc[:,:2]
+df.dropna()
+x
 
 
-# In[1 b.]:
+# In[1 b.]:selecting meaningful features
 
 
 from sklearn import preprocessing
@@ -66,7 +55,7 @@ plt.figure(figsize=(10,6))
 sns.heatmap(cor,annot=True)
 
 
-# In[2 a.]:
+# In[2 a.]:k fold
 
 
 from numpy import array
@@ -162,7 +151,7 @@ plt.legend(loc="best")
 plt.show()
 
 
-# In[2 b.]:
+# In[2 b.]:grod search
 
 
 #grid search
@@ -196,7 +185,7 @@ best_result = gd_sr.best_score_
 print(best_result)
 
 
-# In[3]:
+# In[3]:pca
 
 
 import numpy as np
@@ -246,7 +235,7 @@ plt.title('LDA of IRIS dataset')
 plt.show()
 
 
-# In[4]:
+# In[4]:data clustering
 
 
 from sklearn import datasets
@@ -298,7 +287,7 @@ model.compile(loss='categorical_crossentropy',metrics=['accuracy'],optimizer='ad
 history=model.fit(X_train,Y_train,batch_size=128,epochs=10,validation_data=(X_test,Y_test))
 
 
-# In[5]:
+# In[5]:classification model
 
 
 from sklearn import datasets
@@ -358,7 +347,7 @@ centers = kmeans.cluster_centers_
 plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
 
 
-# In[6]:
+# In[6]: vector
 
 
 import numpy as NP
